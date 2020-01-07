4d1469f1fd39db5f81090aa4030e02b8e7f456f0
Luca Iaconelli <lucabelli@msn.com>
2020-01-07 11:18:25 +0100

    chore: added examples

--- src/VApp/VApp.vue
+++ src/VApp/VApp.vue
@@ -5,13 +5,95 @@
 
             <div class="grid-cell left">
                 <aside>
-                    <section>
+                    <section class="doodle">
 
                         <vue-css-doodle
+                            v-once
                             absolute
                             use="var(--ruleOne)"
                             click-to-update
+                            demo-one
                         />
+
+                    </section>
+                    <section class="doodle">
+
+                        <vue-css-doodle
+                            v-once
+                            absolute
+                            use="var(--ruleTwo)"
+                            click-to-update
+                            demo-two
+                        />
+
+                    </section>
+                    <section class="doodle">
+
+                        <vue-css-doodle
+                            v-once
+                            absolute
+                            click-to-update
+                        >
+                            <!-- eslint-disable -->
+                            --translate: translateY(calc(-66vmin / @size() * @i()));
+
+                            :doodle {
+                                @grid: 45x1 / 100vmax;
+                            }
+
+                            :container {
+                                transform: translateY( 100% );
+                            }
+
+                            :after,
+                            :before {
+                                content: '';
+                                @place-cell: center;
+                                @size: 100%;
+                                background: radial-gradient( @p(#feca57, #e74c3c) @r(70%), transparent 0 ) @pn(30% 50%, 70% 50%, 50% 60%) / @r(.1vmin, 5vmin) @lr() no-repeat;
+                            }
+
+                            @place-cell: center;
+                            @size: 100%;
+
+                            will-change: transform;
+                            animation: r 4s linear infinite;
+                            animation-delay: calc(-4s / @size() * @i());
+
+                            @keyframes r {
+                                from { transform: var(--translate) rotate(0) }
+                                to { transform: var(--translate) rotateZ(-1turn) }
+                            }
+                            <!-- eslint-enable -->
+                        </vue-css-doodle>
+
+                    </section>
+                    <section class="doodle">
+
+                        <vue-css-doodle
+                            v-once
+                            absolute
+                            click-to-update
+                        >
+                            <!-- eslint-disable -->
+                            :doodle {
+                                @grid: 1x3 / 100vmax;
+                            }
+
+                            @size: 100% 150%;
+                            position: absolute;
+
+                            background: @m(100, ( linear-gradient( transparent, @p(#feca57@repeat(2, @p([0-9a-f])), #e74c3c@repeat(2, @p([0-9a-f]))) ) @r(0%, 100%) @r(0%, 100%) / @r(1px) @r(23vmin) no-repeat ));
+
+                            will-change: transform;
+                            animation: f 20s linear calc(-20s / @size() * @i()) infinite;
+
+                            @keyframes f {
+                                from { transform: translateY(-100%) }
+                                to { transform: translateY(100%) }
+                            }
+                            <!-- eslint-enable -->
+                        </vue-css-doodle>
 
                     </section>
                 </aside>
@@ -105,6 +187,12 @@
     scoped
     lang="scss"
     src="./style.scss"
+></style>
+
+<style
+    scoped
+    lang="scss"
+    src="./style-demo.scss"
 ></style>
 
 <style
